89f1026578d8ff401daa22de19380ab94247e85d
Elmaddin Asadov <elmaddinasger@gmail.com>
2024-05-22 11:07:55 +0400

    First week first task.

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,48 @@
-fun main() {
-    println("Hello World!")
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fun plusMinus(arr: Array<Int>): Unit {
+    // Write your code here
+    var plus = 0
+    var minus = 0
+    var zero = 0
+    for (i in arr) {
+        if (i > 0) plus++
+        else if (i < 0) minus++
+        else zero++
+    }
+    println(plus/arr.size.toFloat())
+    println(minus/arr.size.toFloat())
+    println(zero/arr.size.toFloat())
+
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    plusMinus(arr)
 }
